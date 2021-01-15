a9968ca8da65d7c46128f04cab1a9b5f526ea5b1
luigigutierrez23 <luigijose.gutierrez@gmail.com>
2021-01-15 15:34:32 -0300

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+using API.AxConnectorServices;
+
+namespace API.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Ber_Lg_ServicesConnectorTestClient clientAX = new Ber_Lg_ServicesConnectorTestClient();
+        CallContext callContext = new CallContext();
+
+        [HttpGet]
+        public string getProducts()
+        {
+            var x = clientAX.getTextAX(callContext);
+            return x;
+        }
+    }
+}
